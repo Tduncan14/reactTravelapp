c7cadb35196fb23467d35ca2e4ffb081b70f0304
Tduncan14 <nbatre1@yahoo.com>
2020-10-23 23:57:51 -0700

    keep moving forward

---
+++ travelapp/src/components/Card.js
@@ -0,0 +1,18 @@
+import react from 'react';
+
+
+
+
+function Cards(){
+
+
+    return(
+        <div className="cards">
+            <h1>Check out the destinations</h1>
+
+            <div className="cards_container">
+                
+            </div>
+        </div>
+    )
+}
---
+++ travelapp/src/components/CardItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+
+function CardItem(){
+
+
+  return(
+      <>
+      
+      <li className="cards_item">
+          <Link className ="cards_item_link">
+              <figure className="cards_item_pic-wrap">
+                  <img src="/" alt="travel-iamge" className="cards_item_img"/>
+      
+      
+              </figure>
+
+              <div className="cards_item_info">
+            <h5 className="cards_item_text"></h5>
+              </div>
+          </Link>
+      </li>
+
+
+      </>
+  )
+
+
+}
+
+export default CardItem;
